4e2fd1249e774ec5c37f1f41e6949f45c9b063be
Daniel Llorens <daniel.llorens@bluewin.ch>
2013-04-12 17:50:09 +0200

    Use underlying vector implementation directly in array handles
    * libguile/array-handle.c
      - scm_array_get_handle: if the object is an array, point impl to
        the underlying vector instead of array impl, then fix the axes. Avoid
        calling scm_i_array_implementation_for_obj twice.
    * libguile/arrays.c
      - array_handle_ref, array_handle_set, array_get_handle: remove.
    * libguile/bitvectors.c, libguile/bytevectors.c, libguile/strings.c,
      libguile/vectors.c: fix base = 0 in the array handle.
    * libguile/vectors.c: (vector_handle_set, vector_handle_ref): do not
      use h->dims.

--- libguile/array-map.c
+++ libguile/array-map.c
@@ -788,7 +788,7 @@ array_index_map_1 (SCM ra, SCM proc)
   v = h.array;
   inc = h.dims[0].inc;
   for (i = h.dims[0].lbnd, p = h.base; i <= h.dims[0].ubnd; ++i, p += inc)
-    h.impl->vset (&h, p, scm_call_1 (proc, scm_from_ulong (i)));
+    h.impl->vset (h.array, p, scm_call_1 (proc, scm_from_ulong (i)));
   scm_array_handle_release (&h);
 }
 
