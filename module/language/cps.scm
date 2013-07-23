b643526d7dbae6057130e03eedfb06c0909511e2
Andy Wingo <wingo@pobox.com>
2013-07-23 15:51:35 +0200

    Add CPS language
    * module/Makefile.am:
    * module/language/cps.scm:
    * module/language/cps/verify.scm: Add CPS language.

---
+++ module/language/cps.scm
@@ -0,0 +1,232 @@
+;;; Continuation-passing style (CPS) intermediate language (IL)
+
+;; Copyright (C) 2013 Free Software Foundation, Inc.
+
+;;;; This library is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU Lesser General Public
+;;;; License as published by the Free Software Foundation; either
+;;;; version 3 of the License, or (at your option) any later version.
+;;;;
+;;;; This library is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with this library; if not, write to the Free Software
+;;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+;;; Commentary:
+;;;
+;;; This is the continuation-passing style (CPS) intermediate language
+;;; (IL) for Guile.  It is a lower-level representation than Tree-IL, in
+;;; that it assumes that some Tree-IL concepts have already been
+;;; compiled away.  For example, CPS fixes the order of evaluation,
+;;; requires that Scheme's <letrec> has already been lowered to <fix>,
+;;; inlines default-value initializers into lambda-case expressions, and
+;;; inlines prompt bodies.
+;;;
+;;; The utility of CPS is that it gives a name to everything: every
+;;; intermediate value, and every control point (continuation).  As such
+;;; it is more verbose than Tree-IL, but at the same time more simple as
+;;; the number of concepts is reduced.
+;;;
+;;; There are two kinds of terms in CPS: terms that bind continuations,
+;;; and terms that call continuations.  $letk binds a set of mutually
+;;; recursive continuations, each one an instance of $cont.  A $cont
+;;; declares the name and source of a continuation, and then contains as
+;;; a subterm the particular continuation instance: $kif for test
+;;; continuations, $kargs for continuations that bind values, etc.
+;;;
+;;; $continue nodes call continuations.  The expression contained in the
+;;; $continue node determines the value or values that are passed to the
+;;; target continuation: $const to pass a constant value, $values to
+;;; pass multiple named values, etc.
+;;;
+;;; Code:
+
+(define-module (language cps)
+  #:use-module (ice-9 match)
+  #:use-module ((srfi srfi-1) #:select (fold))
+  #:use-module (srfi srfi-9)
+  #:use-module (srfi srfi-9 gnu)
+  #:export (;; Continuations.
+            $letk $cont $kif $ktrunc $kargs $kentry
+
+            ;; Calls.
+            $continue
+            $var $void $const $prim
+            $fun $arity $letrec
+            $call $primcall $values $prompt
+
+            ;; Constructors.
+            make-$letk make-$cont
+            make-$kif make-$ktrunc make-$kargs make-$kentry
+
+            make-$fun make-$arity make-$letrec
+
+            make-$continue
+            make-$var make-$void make-$const make-$prim
+            make-$call make-$primcall make-$values make-$prompt
+
+            parse-cps
+            unparse-cps))
+
+;; FIXME: Use SRFI-99, when Guile adds it.
+(define-syntax define-record-type*
+  (lambda (x)
+    (define (id-append ctx . syms)
+      (datum->syntax ctx (apply symbol-append (map syntax->datum syms))))
+    (syntax-case x ()
+      ((_ name field ...)
+       (and (identifier? #'name) (and-map identifier? #'(field ...)))
+       (with-syntax ((cons (id-append #'name #'make- #'name))
+                     (pred (id-append #'name #'name #'?))
+                     ((getter ...) (map (lambda (f)
+                                          (id-append f #'name #'- f))
+                                        #'(field ...))))
+         #'(define-record-type name
+             (cons field ...)
+             pred
+             (field getter)
+             ...))))))
+
+(define-syntax-rule (define-cps-type name field ...)
+  (begin
+    (define-record-type* name field ...)
+    (set-record-type-printer! name print-cps)))
+
+(define (print-cps exp port)
+  (format port "#<cps ~S>" (unparse-cps exp)))
+
+;; Helper.
+(define-record-type* $arity req opt rest kw allow-other-keys?)
+
+;; Continuations.
+(define-cps-type $letk conts body)
+(define-cps-type $cont src k cont)
+(define-cps-type $kif kt kf)
+(define-cps-type $ktrunc arity k)
+(define-cps-type $kargs names syms body)
+(define-cps-type $kentry arity cont alternate)
+
+;; Calls.
+(define-cps-type $continue k exp)
+(define-cps-type $var sym)
+(define-cps-type $void)
+(define-cps-type $const val)
+(define-cps-type $prim name)
+(define-cps-type $fun meta self free body)
+(define-cps-type $letrec names syms funs body)
+(define-cps-type $call proc args)
+(define-cps-type $primcall name args)
+(define-cps-type $values args)
+(define-cps-type $prompt escape? tag handler)
+
+(define (parse-cps exp)
+  (define (src exp)
+    (let ((props (source-properties exp)))
+      (and (pair? props) props)))
+  (match exp
+    ;; Continuations.
+    (('let k (name sym val) body)
+     (make-$letk (list (make-$cont (src exp) k
+                                (make-$kargs (list name) (list sym)
+                                             (parse-cps body))))
+                 (parse-cps val)))
+    (('letk (cont ...) body)
+     (make-$letk (map parse-cps cont) (parse-cps body)))
+    (('k sym body)
+     (make-$cont (src exp) sym (parse-cps body)))
+    (('kif kt kf)
+     (make-$kif kt kf))
+    (('ktrunc req rest k)
+     (make-$ktrunc (make-$arity req '() rest '() #f) k))
+    (('kargs names syms body)
+     (make-$kargs names syms (parse-cps body)))
+    (('kentry (req opt rest kw allow-other-keys?) body . tail)
+     (make-$kentry (make-$arity req opt rest kw allow-other-keys?)
+                   (parse-cps body)
+                   (match tail
+                     ((alternate) (parse-cps alternate))
+                     (() #f))))
+    (('kseq body)
+     (make-$kargs '() '() (parse-cps body)))
+
+    ;; Calls.
+    (('continue k exp)
+     (make-$continue k (parse-cps exp)))
+    (('var sym)
+     (make-$var sym))
+    (('void)
+     (make-$void))
+    (('const exp)
+     (make-$const exp))
+    (('prim name)
+     (make-$prim name))
+    (('fun meta self free body)
+     (make-$fun meta self free (parse-cps body)))
+    (('letrec ((name sym fun) ...) body)
+     (make-$letrec name sym (map parse-cps fun) (parse-cps body)))
+    (('call proc arg ...)
+     (make-$call proc arg))
+    (('primcall name arg ...)
+     (make-$primcall name arg))
+    (('values arg ...)
+     (make-$values arg))
+    (('prompt escape? tag handler)
+     (make-$prompt escape? tag handler))
+    (_
+     (error "unexpected cps" exp))))
+
+(define (unparse-cps exp)
+  (match exp
+    ;; Continuations.
+    (($ $letk (($ $cont src k ($ $kargs (name) (sym) body))) val)
+     `(let ,k (,name ,sym ,(unparse-cps val))
+           ,(unparse-cps body)))
+    (($ $letk conts body)
+     `(letk ,(map unparse-cps conts) ,(unparse-cps body)))
+    (($ $cont src sym body)
+     `(k ,sym ,(unparse-cps body)))
+    (($ $kif kt kf)
+     `(kif ,kt ,kf))
+    (($ $ktrunc ($ $arity req () rest '() #f) k)
+     `(ktrunc ,req ,rest ,k))
+    (($ $kargs () () body)
+     `(kseq ,(unparse-cps body)))
+    (($ $kargs names syms body)
+     `(kargs ,names ,syms ,(unparse-cps body)))
+    (($ $kentry ($ $arity req opt rest kw allow-other-keys?) body alternate)
+     `(kentry (,req ,opt ,rest ,kw ,allow-other-keys?)
+              ,(unparse-cps body)
+              ,@(if alternate (list (unparse-cps alternate)) '())))
+
+    ;; Calls.
+    (($ $continue k exp)
+     `(continue ,k ,(unparse-cps exp)))
+    (($ $var sym)
+     `(var ,sym))
+    (($ $void)
+     `(void))
+    (($ $const val)
+     `(const ,val))
+    (($ $prim name)
+     `(prim ,name))
+    (($ $fun meta self free body)
+     `(fun ,meta ,self ,free ,(unparse-cps body)))
+    (($ $letrec names syms funs body)
+     `(letrec ,(map (lambda (name sym fun)
+                      (list name sym (unparse-cps fun)))
+                    names syms funs)
+        ,(unparse-cps body)))
+    (($ $call proc args)
+     `(call ,proc ,@args))
+    (($ $primcall name args)
+     `(primcall ,name ,@args))
+    (($ $values args)
+     `(values ,@args))
+    (($ $prompt escape? tag handler)
+     `(prompt ,escape? ,tag ,handler))
+    (_
+     (error "unexpected cps" exp))))
---
+++ module/language/cps/verify.scm
@@ -0,0 +1,170 @@
+;;; Continuation-passing style (CPS) intermediate language (IL)
+
+;; Copyright (C) 2013 Free Software Foundation, Inc.
+
+;;;; This library is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU Lesser General Public
+;;;; License as published by the Free Software Foundation; either
+;;;; version 3 of the License, or (at your option) any later version.
+;;;;
+;;;; This library is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with this library; if not, write to the Free Software
+;;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+;;; Commentary:
+;;;
+;;;
+;;; Code:
+
+(define-module (language cps verify)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-26)
+  #:use-module (language cps)
+  #:export (verify-cps))
+
+(define (verify-cps exp)
+  (define seen-gensyms (make-hash-table))
+  (define (add sym env)
+    (if (hashq-ref seen-gensyms sym)
+        (error "duplicate gensym" sym)
+        (begin
+          (hashq-set! seen-gensyms sym #t)
+          (cons sym env))))
+  (define (add-env new env)
+    (if (null? new)
+        env
+        (add-env (cdr new) (add (car new) env))))
+  (define (check-var sym env)
+    (cond
+     ((not (hashq-ref seen-gensyms sym))
+      (error "unbound lexical" sym))
+     ((not (memq sym env))
+      (error "displaced lexical" sym))))
+  (define (check-src src)
+    (if (and src (not (and (list? src) (and-map pair? src)
+                           (and-map symbol? (map car src)))))
+        (error "bad src")))
+
+  (let visit ((exp exp)
+              (k-env '())
+              (v-env '()))
+    (match exp
+      ;; Continuations.
+      (($ $letk (($ $cont src k cont) ...) body)
+       (let ((k-env (add-env k k-env)))
+         (for-each check-src src)
+         (for-each (match-lambda
+                    (($ $kif kt kf)
+                     (check-var kt k-env)
+                     (check-var kf k-env))
+                    (($ $ktrunc ($ $arity req () rest () #f) k)
+                     (unless (and (list? req) (and-map symbol? req))
+                       (error "bad arity req" req))
+                     (when rest
+                       (unless (symbol? rest)
+                         (error "bad arity rest" rest)))
+                     (check-var k k-env))
+                    (($ $kargs names syms body)
+                     (unless (and (list? names) (and-map symbol? names))
+                       (error "names should be list of symbols" names))
+                     (unless (list? syms)
+                       (error "syms should be list" names))
+                     (visit body k-env (add-env syms v-env)))
+                    ;; $kentry is only ever seen in $fun.
+                    )
+                   cont)
+         (visit body k-env v-env)))
+
+      ;; Calls.
+      (($ $continue k exp)
+       (check-var k k-env)
+       (match exp
+         (($ $var sym)
+          (check-var sym v-env))
+         (($ $void)
+          #t)
+         (($ $const val)
+          #t)
+         (($ $prim name)
+          (unless (symbol? name) (error "name should be a symbol" exp)))
+         (($ $fun meta self free body)
+          (when (and meta (not (and (list? meta) (and-map pair? meta))))
+            (error "meta should be alist" meta))
+          (unless (symbol? self)
+            (error "self should be symbol" exp))
+          (unless (and (list? free) (and-map symbol? list))
+            (error "free should be list of symbols" exp))
+          (unless (symbol? k)
+            (error "entry should be symbol" k))
+          (let lp ((body body))
+            (match body
+              (#f #t)
+              (($ $cont src* k*
+                  ($ $kentry arity ($ $cont src k ($ $kargs names syms body))
+                     alternate))
+               (check-src src*)
+               (check-src src)
+               (match arity
+                 (($ $arity ((? symbol?) ...) ((? symbol?) ...) (or #f (? symbol?))
+                     (((? keyword?)
+                       (? symbol?)
+                       (and (? symbol?) (? (cut memq <> syms))))
+                      ...)
+                     (or #f #t))
+                  #t)
+                 (else (error "bad arity" arity)))
+               (unless (and (list? names) (and-map symbol? names))
+                 (error "letrec names not symbols" exp))
+               (unless (and (list? syms) (and-map symbol? syms))
+                 (error "letrec syms not symbols" exp))
+               (unless (match arity
+                         (($ $arity req opt rest kw allow-other-keys?)
+                          (= (length syms) 
+                             (length names)
+                             (+ (length req)
+                                (length opt)
+                                (if rest 1 0)
+                                ;; FIXME: technically possible for kw syms to
+                                ;; alias other syms
+                                (length kw)))))
+                 (error "unexpected fun-case syms" exp))
+               ;; The continuation environment is null, because we don't turn
+               ;; captured continuations into closures.
+               (visit body (add-env (list k* k) '())
+                      (add-env (cons self syms) v-env))
+               (lp alternate)))))
+         (($ $letrec names syms funs body)
+          (unless (and (list? names) (and-map symbol? names))
+            (error "letrec names not symbols" exp))
+          (unless (and (list? syms) (and-map symbol? syms))
+            (error "letrec syms not symbols" exp))
+          (match funs
+            ((($ $fun) ...) 'ok)
+            (_ (error "letrec funs not lambdas" exp)))
+          (unless (= (length syms) (length names) (length funs))
+            (error "letrec syms, names, and funs not same length" exp))
+          (let ((v-env (add-env syms v-env)))
+            (for-each (cut visit <> k-env v-env) funs)
+            (visit body k-env v-env)))
+         (($ $call proc args)
+          (check-var proc v-env)
+          (for-each (cut check-var <> v-env) args))
+         (($ $primcall name args)
+          (for-each (cut check-var <> v-env) args))
+         (($ $values args)
+          (for-each (cut check-var <> v-env) args))
+         (($ $prompt escape? tag handler)
+          (unless (boolean? escape?) (error "escape? should be boolean" escape?))
+          (check-var tag v-env)
+          (check-var handler k-env))
+         (_
+          (error "unexpected expression" exp))))
+
+      (_
+       (error "unexpected cps" exp)))
+    exp))
