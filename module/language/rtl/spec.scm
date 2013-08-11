228eb0d80576ac0874e228fa331e943ffcf1e6e6
Andy Wingo <wingo@pobox.com>
2013-08-11 14:06:15 +0200

    RTL language
     * module/Makefile.am
     * module/language/rtl.scm:
     * module/language/rtl/spec.scm: Add a stub RTL language.

---
+++ module/language/rtl/spec.scm
@@ -0,0 +1,30 @@
+;;; Register Transfer Language (RTL)
+
+;; Copyright (C) 2013 Free Software Foundation, Inc.
+
+;;;; This library is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU Lesser General Public
+;;;; License as published by the Free Software Foundation; either
+;;;; version 3 of the License, or (at your option) any later version.
+;;;; 
+;;;; This library is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+;;;; 
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with this library; if not, write to the Free Software
+;;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+;;; Code:
+
+(define-module (language rtl spec)
+  #:use-module (system base language)
+  #:export (rtl))
+
+(define-language rtl
+  #:title	"Register Transfer Language"
+  #:compilers   '()
+  #:printer	write
+  #:reader      read
+  #:for-humans? #f)
